f1908c8ebe8a34b069abc1230ae388c41f1f0098
Tony Ropson <Tony_Ropson@Dell.com>
2022-10-08 14:05:21 -0500

    utils work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+mod utils;
 fn main() {
     println!("Hello, world!");
-
+   let _r :i64 = utils::reduce_mod_pm(10,20);
     
 }
--- src/utils.rs
+++ src/utils.rs
@@ -10,14 +10,40 @@
         - a / 2  < r <= a / 2
 
 */
-pub fn reduce_mod_pm(n: i64,a : i64)  -> i64 {
+pub fn reduce_mod_pm(n: i64,a : i64) -> i64 {
    let mut r: i64 = n % a;
-    if (r > (a >> 1)){
+    if r > (a >> 1){
     
         r -= a;
         return r;
+    } else {
+        return 0
     }
     
      
+
+}
+
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+   /* #[test]
+    fn testreduce() {
+       let n:i64 = 10;
+        let a:i64 = 20;
+
+       let r :i64 =  reduce_mod_pm(n,a);
+
+        let z   = n % a;
+        let y = r % a;
+        assert_eq!(z,y);
+    }
+
+*/ 
+
+
 
 }
